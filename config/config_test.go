8e15e9ceb64705cc769f44a61a27122cb420eb29
gyuguen <gyuguen.jang@medibloc.org>
2022-03-22 17:10:13 +0900

    fix

--- config/config_test.go
+++ config/config_test.go
@@ -17,6 +17,8 @@ func TestMustLoad(t *testing.T) {
 	t.Setenv("EDG_DATAVAL_AWS_S3_REGION", "ap-northeast-2")
 	t.Setenv("EDG_DATAVAL_AWS_S3_ACCESS_KEY_ID", "my-access-key")
 	t.Setenv("EDG_DATAVAL_AWS_S3_SECRET_ACCESS_KEY", "my-secret-access-key")
+	t.Setenv("EDG_DATAVAL_CERTIFICATE_STORE_PATH", "my-certificate-store-path")
+	t.Setenv("EDG_DATAVAL_ATTESTATION_PROVIDER_URL", "my-attestation-provider-url")
 
 	c := config.MustLoad()
 	require.Equal(t, log.DebugLevel, log.Level(c.LogLevel))
@@ -27,6 +29,8 @@ func TestMustLoad(t *testing.T) {
 	require.Equal(t, "ap-northeast-2", c.AWSS3Region)
 	require.Equal(t, "my-access-key", c.AWSS3AccessKeyID)
 	require.Equal(t, "my-secret-access-key", c.AWSS3SecretAccessKey)
+	require.Equal(t, "my-certificate-store-path", c.CertificateStorePath)
+	require.Equal(t, "my-attestation-provider-url", c.AttestationProviderURL)
 }
 
 func TestMustLoad_MissingRequiredEnv_HttpAddr(t *testing.T) {
@@ -37,6 +41,8 @@ func TestMustLoad_MissingRequiredEnv_HttpAddr(t *testing.T) {
 	t.Setenv("EDG_DATAVAL_AWS_S3_REGION", "ap-northeast-2")
 	t.Setenv("EDG_DATAVAL_AWS_S3_ACCESS_KEY_ID", "my-access-key")
 	t.Setenv("EDG_DATAVAL_AWS_S3_SECRET_ACCESS_KEY", "my-secret-access-key")
+	t.Setenv("EDG_DATAVAL_CERTIFICATE_STORE_PATH", "my-certificate-store-path")
+	t.Setenv("EDG_DATAVAL_ATTESTATION_PROVIDER_URL", "my-attestation-provider-url")
 
 	require.Panics(t, func() {
 		config.MustLoad()
@@ -51,6 +57,8 @@ func TestMustLoad_MissingRequiredEnv_GrpcAddr(t *testing.T) {
 	t.Setenv("EDG_DATAVAL_AWS_S3_REGION", "ap-northeast-2")
 	t.Setenv("EDG_DATAVAL_AWS_S3_ACCESS_KEY_ID", "my-access-key")
 	t.Setenv("EDG_DATAVAL_AWS_S3_SECRET_ACCESS_KEY", "my-secret-access-key")
+	t.Setenv("EDG_DATAVAL_CERTIFICATE_STORE_PATH", "my-certificate-store-path")
+	t.Setenv("EDG_DATAVAL_ATTESTATION_PROVIDER_URL", "my-attestation-provider-url")
 
 	require.Panics(t, func() {
 		config.MustLoad()
@@ -65,6 +73,8 @@ func TestMustLoad_MissingRequiredEnv_ValidatorMnemonic(t *testing.T) {
 	t.Setenv("EDG_DATAVAL_AWS_S3_REGION", "ap-northeast-2")
 	t.Setenv("EDG_DATAVAL_AWS_S3_ACCESS_KEY_ID", "my-access-key")
 	t.Setenv("EDG_DATAVAL_AWS_S3_SECRET_ACCESS_KEY", "my-secret-access-key")
+	t.Setenv("EDG_DATAVAL_CERTIFICATE_STORE_PATH", "my-certificate-store-path")
+	t.Setenv("EDG_DATAVAL_ATTESTATION_PROVIDER_URL", "my-attestation-provider-url")
 
 	require.Panics(t, func() {
 		config.MustLoad()
@@ -79,6 +89,8 @@ func TestMustLoad_MissingRequiredEnv_AWSS3Bucket(t *testing.T) {
 	t.Setenv("EDG_DATAVAL_AWS_S3_REGION", "ap-northeast-2")
 	t.Setenv("EDG_DATAVAL_AWS_S3_ACCESS_KEY_ID", "my-access-key")
 	t.Setenv("EDG_DATAVAL_AWS_S3_SECRET_ACCESS_KEY", "my-secret-access-key")
+	t.Setenv("EDG_DATAVAL_CERTIFICATE_STORE_PATH", "my-certificate-store-path")
+	t.Setenv("EDG_DATAVAL_ATTESTATION_PROVIDER_URL", "my-attestation-provider-url")
 
 	require.Panics(t, func() {
 		config.MustLoad()
@@ -93,11 +105,47 @@ func TestMustLoad_MissingRequiredEnv_AWSS3Region(t *testing.T) {
 	t.Setenv("EDG_DATAVAL_AWS_S3_BUCKET", "my-s3-bucket")
 	t.Setenv("EDG_DATAVAL_AWS_S3_ACCESS_KEY_ID", "my-access-key")
 	t.Setenv("EDG_DATAVAL_AWS_S3_SECRET_ACCESS_KEY", "my-secret-access-key")
+	t.Setenv("EDG_DATAVAL_CERTIFICATE_STORE_PATH", "my-certificate-store-path")
+	t.Setenv("EDG_DATAVAL_ATTESTATION_PROVIDER_URL", "my-attestation-provider-url")
 
 	require.Panics(t, func() {
 		config.MustLoad()
 	})
 }
+
+func TestMustLoad_MissingRequiredEnv_CertificateStorePath(t *testing.T) {
+	t.Setenv("EDG_DATAVAL_LOG_LEVEL", "debug")
+	t.Setenv("EDG_DATAVAL_HTTP_LADDR", "0.0.0.0:8181")
+	t.Setenv("EDG_DATAVAL_PANACEA_GRPC_ADDR", "0.0.0.0:9191")
+	t.Setenv("EDG_DATAVAL_VALIDATOR_MNEMONIC", "Your MNEMONIC")
+	t.Setenv("EDG_DATAVAL_AWS_S3_REGION", "ap-northeast-2")
+	t.Setenv("EDG_DATAVAL_AWS_S3_BUCKET", "my-s3-bucket")
+	t.Setenv("EDG_DATAVAL_AWS_S3_ACCESS_KEY_ID", "my-access-key")
+	t.Setenv("EDG_DATAVAL_AWS_S3_SECRET_ACCESS_KEY", "my-secret-access-key")
+	t.Setenv("EDG_DATAVAL_ATTESTATION_PROVIDER_URL", "my-attestation-provider-url")
+
+	require.Panics(t, func() {
+		config.MustLoad()
+	})
+}
+
+func TestMustLoad_MissingRequiredEnv_AttestationProviderURL(t *testing.T) {
+	t.Setenv("EDG_DATAVAL_LOG_LEVEL", "debug")
+	t.Setenv("EDG_DATAVAL_HTTP_LADDR", "0.0.0.0:8181")
+	t.Setenv("EDG_DATAVAL_PANACEA_GRPC_ADDR", "0.0.0.0:9191")
+	t.Setenv("EDG_DATAVAL_VALIDATOR_MNEMONIC", "Your MNEMONIC")
+	t.Setenv("EDG_DATAVAL_AWS_S3_REGION", "ap-northeast-2")
+	t.Setenv("EDG_DATAVAL_AWS_S3_BUCKET", "my-s3-bucket")
+	t.Setenv("EDG_DATAVAL_AWS_S3_ACCESS_KEY_ID", "my-access-key")
+	t.Setenv("EDG_DATAVAL_AWS_S3_SECRET_ACCESS_KEY", "my-secret-access-key")
+	t.Setenv("EDG_DATAVAL_CERTIFICATE_STORE_PATH", "my-certificate-store-path")
+
+	require.Panics(t, func() {
+		config.MustLoad()
+	})
+}
+
+
 
 func TestMustLoad_InvalidLogLevel(t *testing.T) {
 	t.Setenv("EDG_DATAVAL_LOG_LEVEL", "hello")
@@ -106,6 +154,8 @@ func TestMustLoad_InvalidLogLevel(t *testing.T) {
 	t.Setenv("EDG_DATAVAL_VALIDATOR_MNEMONIC", "Your MNEMONIC")
 	t.Setenv("EDG_DATAVAL_AWS_S3_ACCESS_KEY_ID", "my-access-key")
 	t.Setenv("EDG_DATAVAL_AWS_S3_SECRET_ACCESS_KEY", "my-secret-access-key")
+	t.Setenv("EDG_DATAVAL_CERTIFICATE_STORE_PATH", "my-certificate-store-path")
+	t.Setenv("EDG_DATAVAL_ATTESTATION_PROVIDER_URL", "my-attestation-provider-url")
 
 	require.Panics(t, func() {
 		config.MustLoad()
