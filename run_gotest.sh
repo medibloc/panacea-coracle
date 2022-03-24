3a1c1f4a852e6951003d4d39971a7884eed6d835
Youngjoon Lee <yjlee@medibloc.org>
2022-03-24 07:12:52 +0000

    fix

--- run_gotest.sh
+++ run_gotest.sh
@@ -8,23 +8,23 @@ echo "GOBIN: ${GOBIN}"
 
 # NOTE: Please update this array if necessary
 PKG_PREFIX=$(grep '^module ' ./go.mod | awk "{print \$2}")
-TEST_PKGS_WITH_EGO=(
+ENCLAVE_TEST_PKGS=(
     "${PKG_PREFIX}/server/tee"
 )
 
 #####################################################################
-echo "[Step 1] Running unit tests that don't require EGo..."
+echo "[Step 1] Running non-enclave tests..."
 
-TEST_PKGS_WITHOUT_EGO=$(go list ./... | grep -v /e2e)  # except e2e/*_test.go
-for pkg in ${TEST_PKGS_WITH_EGO[@]}; do
-    TEST_PKGS_WITHOUT_EGO=$(echo "${TEST_PKGS_WITHOUT_EGO}" | grep -v ${pkg})
+NON_ENCLAVE_TEST_PKGS=$(go list ./... | grep -v /e2e)  # except e2e/*_test.go
+for pkg in ${ENCLAVE_TEST_PKGS[@]}; do
+    NON_ENCLAVE_TEST_PKGS=$(echo "${NON_ENCLAVE_TEST_PKGS}" | grep -v ${pkg})
 done
 
-${GOBIN} test -v ${TEST_PKGS_WITHOUT_EGO}
+${GOBIN} test -v ${NON_ENCLAVE_TEST_PKGS}
 
 
 #####################################################################
-echo "[Step 2] Running unit tests that require EGo..."
+echo "[Step 2] Running enclave tests..."
 
 if [ ${GOBIN} != "ego-go" ]; then
     echo "Skipping: EGo disabled"
@@ -35,7 +35,7 @@ fi
 # Compile the binary of each test package but do not run it.
 # Instead, sign the test binary for EGo and run it with the `ego` command.
 # NOTE: We need this 'for' loop because Go doesn't support building a test binary for all packages.
-for pkg in ${TEST_PKGS_WITH_EGO}; do
+for pkg in ${ENCLAVE_TEST_PKGS}; do
     PKG_DIR=${pkg#$PKG_PREFIX}
     ENCLAVE_JSON_PATH="${SCRIPT_DIR}/${PKG_DIR}/testdata/enclave.json"
     TEST_BIN_PATH="./$(jq -c '.exe' -r ${ENCLAVE_JSON_PATH})"
