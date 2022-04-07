a5f6ff734be02d2d6ee7b7b99d951c6afe4721a4
gyuguen <gyuguen.jang@medibloc.org>
2022-04-07 16:34:23 +0900

    fix

--- e2e/init_start_panacea.sh
+++ e2e/init_start_panacea.sh
@@ -12,14 +12,14 @@ panacead init node1 --chain-id ${CHAIN_ID}
 
 # Init accounts
 panacead keys add validator
-panacead add-genesis-account $(panacead keys show validator -a) 100000000umed
+panacead add-genesis-account $(panacead keys show validator -a) 100000000000umed
 panacead gentx validator 1000000umed --commission-rate 0.1 --commission-max-rate 0.2 --commission-max-change-rate 0.01  --min-self-delegation 1 --chain-id ${CHAIN_ID}
 
 echo -e "${E2E_DATA_BUYER_MNEMONIC}\n\n" | panacead keys add buyer -i
-panacead add-genesis-account $(panacead keys show buyer -a) 100000000umed
+panacead add-genesis-account $(panacead keys show buyer -a) 100000000000umed
 
 echo -e "${E2E_DATAVAL_MNEMONIC}\n\n" | panacead keys add dataval -i
-panacead add-genesis-account $(panacead keys show dataval -a) 100000000umed
+panacead add-genesis-account $(panacead keys show dataval -a) 100000000000umed
 
 panacead collect-gentxs
 
